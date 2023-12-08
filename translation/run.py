ca4ea689d1e5c903ec0a8db17e78e58ccc3a2f74
Minseok Choe <choems@outlook.com>
2023-12-08 21:00:17 +0900

    commit

--- translation/run.py
+++ translation/run.py
@@ -431,6 +431,7 @@ def main(main_args = None):
     # print arguments
     args = parser.parse_args(main_args) if (main_args) else parser.parse_args()
 
+    logger.setLevel(logging.DEBUG)
     logger_file_name = os.path.join(args.output_dir, "train.log" if args.do_train else "test.log")
     file_handler = logging.FileHandler(logger_file_name)
     logger.addHandler(file_handler)
